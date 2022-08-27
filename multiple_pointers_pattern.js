33513766c0b4c0260f27e8a44910ca53dbedab33
FAISAL QURESHI <qureshifi@icloud.com>
2022-08-27 15:43:10 +0530

    unique value counter

--- multiple_pointers_pattern.js
+++ multiple_pointers_pattern.js
@@ -8,7 +8,7 @@ function sumZero(array) {
   while (left < right) {
     let sum = array[left] + array[right];
     if (sum === 0) {
-      console.log([array[left], array[right]]);
+      return [array[left], array[right]];
     } else if (sum > 0) {
       right--;
     } else {
@@ -19,3 +19,17 @@ function sumZero(array) {
 
 console.log(sumZero([3, 5, 2, 6, -5, -6, -3, -3]));
 console.log("hello");
+
+//Count unique values in an array
+
+function unique(array) {
+  let i = 0;
+  for (let j = 1; j < array.length; j++) {
+    if (array[i] !== array[j]) {
+      i++;
+      array[i] = array[j];
+    }
+  }
+  return `The array has ${i + 1} unique values`;
+}
+console.log(unique([1, 1, 2, 2, 3, 3, 4, 4, 5, 5]));
